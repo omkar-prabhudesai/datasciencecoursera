9e4103cb5aa2031ab3500d5c93f3750a18abdfc1
omkar-prabhudesai <omkar.prabhudesai@gmail.com>
2016-06-14 14:00:11 +0530

    Final script

--- GettingAndCleaningData/run_analysis.R
+++ GettingAndCleaningData/run_analysis.R
@@ -17,52 +17,51 @@ dfActivityTrain<- fread("./UCI HAR Dataset/train/Y_train.txt")
 
 ##Merge the subject test and subject train data sets created in above step
 dfSubjectAll <- rbind(dfSubjectTest,dfSubjectTest)
-setnames(dfSubjectAll,"V1","subject")
+setnames(dfSubjectAll,"V1","subjectId")
 
 ##Merge the activity train and activity test data
 dfActivityAll <- rbind(dfActivityTest,dfActivityTrain)
-setnames(dfActivityAll,"V1","activityNum")
+setnames(dfActivityAll,"V1","activityId")
 
 ##Merge Test (i.e. X_test and X_train) and Train data
 dtAll <- rbind(dfTest,dfTrain)
 
-dfSubjectAll <- cbind(dfSubjectAll,dfActivityAll)
-dtAll <- cbind(dfSubjectAll,dtAll)
-setkey(dtAll,subject,activityNum)
-
 ## Add column header to above data set
 dfFeatures <- fread("./UCI HAR Dataset/features.txt")
 setnames(dfFeatures,names(dfFeatures),c("featureId","featureName"))
+
 ##Select only those measurement from features which are for mean and std
-dfSelectedFeatures <- dfFeatures[grepl("mean\\(\\)|std\\(\\)",featureName)]
-##To select the features from dtAll tables we will need to map selected
-##feature names to columns starting with V1,V2....Vn because dtAll table
-##contains columns with these names
-vfeatures <- paste0("V",dfSelectedFeatures$featureId)
-columns <- c(key(dtAll),vfeatures)
+dfSelectedFeatures <- dfFeatures[grepl("(mean|std)\\(\\)",featureName)]
+
 #Now select these columns
 library(dplyr)
 dtAll <- select(dtAll,dfSelectedFeatures$featureId)
+#Assign column names
+colnames(dtAll) <- dfSelectedFeatures$featureName
+
+dfSubject_Activity_All <- cbind(dfSubjectAll,dfActivityAll)
+dtAll <- cbind(dfSubject_Activity_All,dtAll)
+setkey(dtAll,subjectId,activityId)
 
 #Read acivity labes from activity_labels.txt
 dfActivityLables <- fread("./UCI HAR Dataset/activity_labels.txt")
-setnames(dfActivityLables,names(dfActivityLables),c("activityNum","activityName"))
+setnames(dfActivityLables,names(dfActivityLables),c("activityId","activityName"))
 
-#merge activities with main table i.e. dtAll 
-dtAll <- merge(dtAll,dfActivityLables, by="activityNum",all.x = T)
-setkey(dtAll,subject,activityNum,activityName)
+dtAll <- merge(dtAll,dfActivityLables, by="activityId",all.x = T)
+setkey(dtAll,subjectId,activityId,activityName)
 
 #reshape the table
-dtAll <- melt(dtAll,key(dtAll),variable.name = "featureId")
-dtAll <- merge(dtAll , dfSelectedFeatures[, list(featureId, featureName)], by="featureId", all.x=TRUE)
+dtAll <- melt(dtAll,key(dtAll),variable.name = "featureName")
 
 #Cleanup feature names
 dtAll$featureName <- gsub('-mean',"Mean",dtAll$featureName)
 dtAll$featureName <- gsub('[-()]', '', dtAll$featureName)
-dtAll$featureName <- gsub('^t', 'TD_', dtAll$featureName)
-dtAll$featureName <- gsub('^f', 'FD_', dtAll$featureName)
-#dtAll$featureName <- gsub('Acc', 'Accelerometer', dtAll$featureName)
-#dtAll$featureName <- gsub('Gyro', 'Gyroscope', dtAll$featureName)
+dtAll$featureName <- gsub('^t', 'Time', dtAll$featureName)
+dtAll$featureName <- gsub('^f', 'Freq', dtAll$featureName)
+dtAll$featureName <- gsub('-std', 'StdDev', dtAll$featureName)
+dtAll$featureName <- gsub('-', '', dtAll$featureName)
+dtAll$featureName <- gsub('BodyBody', 'Body', dtAll$featureName)
+
 dtTidy <- aggregate(dtAll$value,by=list(activity = dtAll$activityName, subject = dtAll$subject, features = dtAll$featureName),FUN=mean)
 colnames(dtTidy)[4] <- "Mean"
 
